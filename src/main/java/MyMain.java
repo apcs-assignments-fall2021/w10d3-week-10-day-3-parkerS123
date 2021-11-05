b295c47126343de8a3905e588798be719a3c27ab
parkerschwantes <23pschwantes@athenian.org>
2021-11-04 20:31:33 -0700

    I need help on merge

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,9 +5,9 @@ public class MyMain {
     // Counts the number of words that starts with 'a' or 'A'
     public static int startsWithA(ArrayList<String> list) {
         int counter = 0;
-        for (int i = 0; i < list.size(); i++){
-            if (list.get(i).charAt(0) == 'a' || list.get(i).charAt(0) == 'A'){
-               counter++;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).charAt(0) == 'a' || list.get(i).charAt(0) == 'A') {
+                counter++;
             }
         }
         return counter;
@@ -15,7 +15,7 @@ public class MyMain {
 
     // Adds a ! to each element in the ArrayList, returns an ArrayList
     public static ArrayList<String> makeExciting(ArrayList<String> list) {
-        for (int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             list.set(i, list.get(i) + "!");
         }
         return list;
@@ -23,7 +23,14 @@ public class MyMain {
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        // for (int i = 0; i > length.list1)
+
+        for (int i = 0; i < list1.size(); i++) {
+            for (int i2 = 0; i2 < list2.size(); i2++) {
+                if (list1.get(i) == list2.get(i2)) {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
@@ -33,27 +40,56 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) {
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i) % 2) != 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     // Unlike the other methods, the input is an array!
     public static ArrayList<Integer> convertToArrayList(int[] arr) {
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+
+        return list;
     }
 
     // Takes two **sorted** ArrayLists and merges them together into one big sorted ArrayList
     // (Hint: you may find it useful to use a while loop, as well as the remove() method).
     public static ArrayList<Integer> merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        // REPLACE WITH YOUR CODE
-        return null;
+        int size2 = list2.size();
+        int index2 = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            while (size2 > 0) {
+                list1.add(list2.get(index2));
+                index2++;
+                size2--;
+            }
+        }
+        //for (int j = 0; j < list1.size(); j++){
+            //if (list1)
+        //}
+
+        return list2;
     }
 
 
 
+
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        ArrayList<Integer> listy = new ArrayList<Integer>();
+        listy.add(4);
+        listy.add(31);
+        listy.add(7);
+        listy.add(100);
+        System.out.println(countOdd(listy));
+
     }
 }
